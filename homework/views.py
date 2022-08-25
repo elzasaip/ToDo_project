03c2fe6ba25198a8a05077e46ee17f5a865d9dc6
ElzaSaipova <elza8901@gmail.com>
2022-08-25 11:01:44 +0400

    init

---
+++ homework/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def page(request):
+    return render (request, 'homework.html')
+
+def text(request):
+    return HttpResponse ('Добро пожаловать в приложение ToDo - Admin) ')
