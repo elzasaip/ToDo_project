a6a47c8d02ce532bcf66c5a4f18fc4d6a8004fe2
ElzaSaipova <elza8901@gmail.com>
2022-08-29 14:46:02 +0400

    created meeting.html and added static files

--- homework/views.py
+++ homework/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
-# Create your views here.
+from .models import ToMeet
 
 
 def page(request):
     return render (request, 'homework.html')
+
+def meeting(request):
+    tomeet_list = ToMeet.objects.all()
+    return render (request, 'meeting.html', 'tomeet_list':tomeet_list)
 
 def text(request):
     return HttpResponse ('Добро пожаловать в приложение ToDo - Admin) ')
